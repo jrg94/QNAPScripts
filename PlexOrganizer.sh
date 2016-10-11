3de90efe1597203565e5905f13222bed44b6373c
jrg94 <jrg170@case.edu>
2016-10-10 21:52:10 -0400

    Moved code into functions

--- PlexOrganizer.sh
+++ PlexOrganizer.sh
@@ -3,9 +3,20 @@
 # 1. Rename files that need to be renamed
 # 2. Move files as appropriate
 PATH_TO_PLEX="X:\To Be Added"
+PLEX_STAGING="X:\Staging"
 
-# List all files in a directory
-ALL_FILES="$(ls -LR "${PATH_TO_PLEX}")"
-echo "${ALL_FILES}"
+# The main function from which this script runs
+main () {
 
-read -n1 -r -p "Press any key to continue" key
+  if [ ! -d $PLEX_STAGING ]; then
+    mkdir $PLEX_STAGING
+  fi
+
+  # List all files in a directory
+  ALL_FILES="$(ls -LR "${PATH_TO_PLEX}")"
+  echo "${ALL_FILES}"
+
+  read -n1 -r -p "Press any key to continue" key
+}
+
+main
