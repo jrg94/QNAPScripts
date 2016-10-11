9b7e7bda56d635e3d1030b48f1086f601f64b824
jrg94 <jrg170@case.edu>
2016-10-10 22:09:43 -0400

    Add staging development

--- PlexOrganizer.sh
+++ PlexOrganizer.sh
@@ -4,13 +4,36 @@
 # 2. Move files as appropriate
 PATH_TO_PLEX="X:\To Be Added"
 PLEX_STAGING="X:\Staging"
+PLEX_VIDEOS=$PLEX_STAGING"\Videos"
+PLEX_MUSIC=$PLEX_STAGING"\Music"
+PLEX_PICTURES=$PLEX_STAGING"\Pictures"
+
+# Makes a director if it exists
+make_dir () {
+  if [ ! -d $1 ]; then
+    mkdir $1
+  fi
+}
+
+# Builds up the staging directory
+build_staging () {
+  # If the staging folder does not exist, create it
+  make_dir $PLEX_STAGING
+
+  # If the staging folder does not have a videos folder, make one
+  make_dir $PLEX_VIDEOS
+
+  # If the staging folder does not have a music folder, make one
+  make_dir $PLEX_MUSIC
+
+  # If the staging folder does not have a pictures folder, make one
+  make_dir $PLEX_PICTURES
+}
 
 # The main function from which this script runs
 main () {
 
-  if [ ! -d $PLEX_STAGING ]; then
-    mkdir $PLEX_STAGING
-  fi
+  build_staging
 
   # List all files in a directory
   ALL_FILES="$(ls -LR "${PATH_TO_PLEX}")"
