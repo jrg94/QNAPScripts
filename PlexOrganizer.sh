a438b54564c4a6ae0c4ccad5eb41320e46254e6a
jrg94 <jrg170@case.edu>
2016-10-10 21:42:59 -0400

    Added file listing functionality

--- PlexOrganizer.sh
+++ PlexOrganizer.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+# Steps to solving the Plex Nightmare
+# 1. Rename files that need to be renamed
+# 2. Move files as appropriate
+PATH_TO_PLEX="X:\To Be Added"
+
+# List all files in a directory
+ALL_FILES="$(ls -LR "${PATH_TO_PLEX}")"
+echo "${ALL_FILES}"
 
 read -n1 -r -p "Press any key to continue" key
